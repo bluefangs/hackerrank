ce502535f98cdb45ff5951f2fc73bee294d51654
Bharath <bharathkeshav12@gmail.com>
2019-02-18 11:42:52 +0530

    Create BonAppetit.js

---
+++ algorithms/BonAppetit.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the bonAppetit function below.
+function bonAppetit(bill, k, b) {
+    const total = bill.reduce((a, b) => a + b, 0);
+    const annaShouldPay = (total-bill[k]) / 2;
+    b != annaShouldPay ? console.log(b - annaShouldPay) : console.log('Bon Appetit')
+
+}
+
+function main() {
+    const nk = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const bill = readLine().replace(/\s+$/g, '').split(' ').map(billTemp => parseInt(billTemp, 10));
+
+    const b = parseInt(readLine().trim(), 10);
+
+    bonAppetit(bill, k, b);
+}
