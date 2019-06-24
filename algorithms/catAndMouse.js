0f1e5525749700dd42f554bcfc581093572655b6
Bharath <bharathkeshav12@gmail.com>
2019-06-24 20:01:07 +0530

    Problem : http://hr.gs/39y9
    http://hr.gs/39y9

---
+++ algorithms/catAndMouse.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the catAndMouse function below.
+function catAndMouse(x, y, z) {
+    let mouseCatA = Math.abs(z - x); // let go of anything negative. Be absolute!
+    let mouseCatB = Math.abs(z - y); // let go of anything negative. Be absolute!   
+
+    if (mouseCatA == mouseCatB) {
+        return 'Mouse C'
+    }
+    return (mouseCatA < mouseCatB) ? 'Cat A' : 'Cat B' 
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const xyz = readLine().split(' ');
+
+        const x = parseInt(xyz[0], 10);
+
+        const y = parseInt(xyz[1], 10);
+
+        const z = parseInt(xyz[2], 10);
+
+        let result = catAndMouse(x, y, z);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
